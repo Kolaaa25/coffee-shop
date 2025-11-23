38d125baded39d80bec74c393f92976697fbff53
Kolaaa25 <matehakola@gmail.com>
2025-11-22 21:50:04 -0600

    Replace bcrypt with bcryptjs to fix Render deployment

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { findUserByEmail, findUserById, createUser } from '../models/users.js';
 import { authenticateToken } from '../middleware/auth.js';
